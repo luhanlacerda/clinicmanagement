e9342a19a12639f2d295fa52841888e154cc2862
Luhan Lacerda <luhanlacerda@gmail.com>
2017-10-06 22:05:25 -0300

    Criacao classes basicas endereco e pessoa

--- src/classesBasicas/Pessoa.java
+++ src/classesBasicas/Pessoa.java
@@ -1,6 +1,89 @@
 package classesBasicas;
 
-public class Pessoa {
+import java.util.Calendar;
 
-	//inicializando
+public abstract class Pessoa {
+
+	private int id;
+	private String nome;
+	private String rg;
+	private String cpf;
+	private Endereco endereco;
+	private String contato;
+	private Calendar dtNascimento;
+	private String email;
+	private String estadoCivil;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getRg() {
+		return rg;
+	}
+
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public Endereco getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getContato() {
+		return contato;
+	}
+
+	public void setContato(String contato) {
+		this.contato = contato;
+	}
+
+	public Calendar getDtNascimento() {
+		return dtNascimento;
+	}
+
+	public void setDtNascimento(Calendar dtNascimento) {
+		this.dtNascimento = dtNascimento;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getEstadoCivil() {
+		return estadoCivil;
+	}
+
+	public void setEstadoCivil(String estadoCivil) {
+		this.estadoCivil = estadoCivil;
+	}
+
 }
