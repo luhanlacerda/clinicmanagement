adf23a23936afe3b17c1b6b84553376fa98e2f8e
Rodrigo Rodrigues <rodrigo7.carvalho.rodrigues@gmail.com>
2017-10-06 22:55:09 -0300

    Adicionando classes basicas Convenio e Paciente

---
+++ src/classesBasicas/Convenio.java
@@ -0,0 +1,27 @@
+package classesBasicas;
+
+public class Convenio {
+
+	private int id;
+	private String descricao;
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+
+	public int getId() {
+
+		return id;
+	}
+	
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+}
---
+++ src/classesBasicas/Paciente.java
@@ -0,0 +1,15 @@
+package classesBasicas;
+
+public class Paciente extends Pessoa {
+
+	private Convenio convenio;
+
+	public Convenio getConvenio() {
+		return convenio;
+	}
+
+	public void setConvenio(Convenio convenio) {
+		this.convenio = convenio;
+	}
+	
+}
