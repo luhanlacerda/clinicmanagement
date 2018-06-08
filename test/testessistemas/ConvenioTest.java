09be64ed13f18f69de2b419a84d0504c812cb1b8
Luhan Lacerda <luhanlacerda@gmail.com>
2018-06-08 15:11:56 -0300

    Teste para inserir convenio com selenium com erros corrigidos

--- test/testessistemas/ConvenioTest.java
+++ test/testessistemas/ConvenioTest.java
@@ -1,14 +1,13 @@
 package testessistemas;
 
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -31,9 +30,8 @@ public class ConvenioTest {
 		driver.close();
 	}
 
-	@Test
+	@Ignore
 	public void testarInserirConvenio() {
-		Actions actions = new Actions(driver);
 		driver.navigate().to(URL);
 
 		WebElement username = driver.findElement(By.id("formulariologin:username"));
@@ -76,6 +74,53 @@ public class ConvenioTest {
 		wait.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("//div[@class='preloader']")));
 		
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//td[contains(text(),'Teste Inserindo Selenium')]")));	
+
+	}
+	
+	@Test
+	public void testarAtualizarConvenio() {
+		driver.navigate().to(URL);
+
+		WebElement username = driver.findElement(By.id("formulariologin:username"));
+		username.sendKeys("Teste");
+
+		WebElement password = driver.findElement(By.id("formulariologin:password"));
+		password.sendKeys("qwerty");
+
+		WebElement btnLogar = driver.findElement(By.id("formulariologin:btnlogar"));
+		btnLogar.click();
+
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='preloader']")));
+
+		wait.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("//div[@class='preloader']")));
+
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.linkText("Convenio")));
+		
+		driver.findElement(By.linkText("Convenio")).click();
+		
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='preloader']")));
+		
+		wait.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("//div[@class='preloader']")));
+		
+		driver.findElement(By.name("j_idt32:j_idt36:0:j_idt45")).click();
+		
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='preloader']")));
+		
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='preloader']")));
+		
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@placeholder='Descrição']")));
+		
+		driver.findElement(By.xpath("//input[@placeholder='Descrição']")).clear();
+		
+		driver.findElement(By.xpath("//input[@placeholder='Descrição']")).sendKeys("Teste Alterando Selenium");
+		
+		driver.findElement(By.xpath("//input[@value='Cadastrar']")).click();
+		
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='preloader']")));
+
+		wait.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("//div[@class='preloader']")));
+		
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//td[contains(text(),'Teste Alterando Selenium')]")));	
 
 	}
 
